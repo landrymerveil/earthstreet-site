6bdc74c27233965726b73286712b985f1ec08081
landrymerveil <landrymerveil2002@gmail.com>
2026-08-25 09:20:05 +0100

    Premier version site Earth Street

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+
+<main>
+    <h2>Contactez-nous</h2>
+    <p><b>Adresse :</b> Abomey-Calavi, Atlantique, Bénin</p>
+    <p><b>Téléphone :</b> +229 01 40 96 33 12 </p>
+    <p><b>Email :</b> contact@earthstreet.bj</p>
+</main>
+
+<?php include 'footer.php'; ?>
---
+++ footer.php
@@ -0,0 +1,7 @@
+    <footer>
+        <h3>APPELEZ-NOUS</h3>
+        <p>01 64 06 26 61 | 01 40 96 33 12</p>
+        <p>Contactez-nous !</p>
+    </footer>
+</body>
+</html>
